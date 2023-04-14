0f3f5669f4234ee79304fbc89baf92d93a60dc47
gulamov-nuriddin <gm.nuriddin2020@gmail.com>
2023-04-14 21:47:43 +0500

    asdf

--- src/components/UI/buttons/ShareButton.js
+++ src/components/UI/buttons/ShareButton.js
@@ -2,7 +2,7 @@ function ShareButton({ Icon, title }) {
   return (
     <button className="p-[10px] flex justify-center items-center w-[100%] hover:bg-gray100 rounded-lg transition duration200 ease">
       <Icon className='text-[22px]' />
-      <p className="ml-[10px] text-gray350 text-[18px]">{title}</p>
+      <p className="ml-[10px] text-gray350 text-[16px]">{title}</p>
     </button>
   );
 }
