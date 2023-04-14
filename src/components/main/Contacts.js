955ebdd69d55224ec1fd62c44ddd86acda93720b
gulamov-nuriddin <gm.nuriddin2020@gmail.com>
2023-04-14 22:53:34 +0500

    sidebar changes

--- src/components/main/Contacts.js
+++ src/components/main/Contacts.js
@@ -1,6 +1,7 @@
-import { useSelector } from 'react-redux';
+import { useSelector } from "react-redux";
 
-import CONTACTS from '../../data/contacts';
+import CONTACTS from "../../data/contacts";
+import SidebarButton from "../UI/buttons/SidebarButton";
 
 function Contacts() {
   const mobileMenuOpened = useSelector((state) => state.mobileMenuOpened);
@@ -8,24 +9,13 @@ function Contacts() {
   return (
     <div
       className={`flex flex-2 flex-col py-[20px] px-[10px] max-[850px]:hidden ${
-        mobileMenuOpened && 'hidden'
+        mobileMenuOpened && "hidden"
       }`}
     >
-      <h2 className="text-[22px] text-gray400 mb-[10px] pl-[10px]">Contacts</h2>
-      {CONTACTS.map((contact, index) => {
-        const Icon = contact.icon;
-
-        return (
-          <button
-            className={`flex items-center p-[10px] hover:bg-gray250 rounded-lg ${
-              index !== 0 && 'mt-[5px]'
-            }`}
-          >
-            <Icon className="text-[20px] text-gray400" />
-            <p className="ml-[10px] text-black100 leading-5 text-left">{contact.name}</p>
-          </button>
-        );
-      })}
+      <h2 className="text-[22px] text-gray400 mb-[10px] pl-[15px]">Contacts</h2>
+      {CONTACTS.map((contact, index) => (
+        <SidebarButton Icon={contact.icon} title={contact.name} />
+      ))}
     </div>
   );
 }
