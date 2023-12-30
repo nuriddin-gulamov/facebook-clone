9a0410f186747049d945a233ff29edadcd82ba46
gulamov-nuriddin <gm.nuriddin2020@gmail.com>
2023-12-30 18:11:18 +0500

    minor code corrections

--- src/store/firebase.js
+++ src/store/firebase.js
@@ -5,9 +5,10 @@ import { getDatabase } from "firebase/database";
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: "facebook-clone-n.firebaseapp.com",
+  databaseURL: "https://facebook-clone-n-default-rtdb.firebaseio.com",
   projectId: "facebook-clone-n",
   storageBucket: "facebook-clone-n.appspot.com",
-  messagingSenderId: process.env.REACT_APP_ID,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_APP_ID,
 };
 
