ac1c0f071629d13ba8e6bf57abb5af135b157e2f
gulamov-nuriddin <gm.nuriddin2020@gmail.com>
2024-10-20 10:09:59 +0500

    minor changes & issues fixed

--- src/data/contacts.js
+++ src/data/contacts.js
@@ -2,18 +2,14 @@ import elon from "../assets/contacts/1.jpg";
 import jeff from "../assets/contacts/2.jpg";
 import steve from "../assets/contacts/3.jpg";
 import obama from "../assets/contacts/4.jpg";
-import andrew from "../assets/contacts/5.jpg";
-import bill from "../assets/contacts/6.jpg";
-import tristan from "../assets/contacts/7.jpg";
+import bill from "../assets/contacts/5.jpg";
 
 const CONTACTS = [
     { img: elon, name: "Elon Musk" },
     { img: jeff, name: "Jeff Bezos" },
     { img: bill, name: "Bill Gates" },
     { img: steve, name: "Steve Jobs" },
-    { img: obama, name: "Barack Obama" },
-    { img: andrew, name: "Andrew Tate" },
-    { img: tristan, name: "Tristan Tate" }
+    { img: obama, name: "Barack Obama" }
 ];
 
 export default CONTACTS;
